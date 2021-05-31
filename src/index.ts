039c4496e984261b0da93ebdd29023fc04dca3a3
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-05-31 13:02:07 +0530

    add : basic crud for post

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
 /** @format */
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
+
+  // server setup
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server started at port 4000");
+  });
 };
 
 main();
