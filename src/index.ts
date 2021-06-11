f1afbdf4be76c1741fd612edcdaee711e44c8856
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-06-11 23:41:28 +0530

    add : typeorm and initial config

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
 /** @format */
 import "reflect-metadata";
 import express from "express";
-import { MikroORM } from "@mikro-orm/core";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import Redis from "ioredis";
 import session from "express-session";
 import connectRedis from "connect-redis";
 import cors from "cors";
-import mikroOrmConfig from "./mikro-orm.config";
+import { createConnection } from "typeorm";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
 import { COOKIE_NAME, __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { User } from "./entities/User";
 
 const main = async () => {
-  const orm = await MikroORM.init(mikroOrmConfig);
-  await orm.getMigrator().up();
+  const conn = await createConnection({
+    type: "postgres",
+    database: "dreddit-2",
+    username: "deepak",
+    password: "dpak",
+    logging: true,
+    synchronize: true,
+    entities: [Post, User],
+  });
 
   // server setup
   const app = express();
@@ -49,7 +57,7 @@ const main = async () => {
       resolvers: [PostResolver, UserResolver],
       validate: false,
     }),
-    context: ({ req, res }) => ({ em: orm.em, req, res, redis }),
+    context: ({ req, res }) => ({ req, res, redis }),
   });
 
   apolloServer.applyMiddleware({
