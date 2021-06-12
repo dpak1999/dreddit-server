648975bf72c68f85683ad43ee559e8b5743bec56
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-06-12 17:50:26 +0530

    add : auth middleware

---
+++ src/middleware/isAuth.ts
@@ -0,0 +1,12 @@
+/** @format */
+
+import { MyContext } from "src/types";
+import { MiddlewareFn } from "type-graphql";
+
+export const isAuth: MiddlewareFn<MyContext> = ({ context }, next) => {
+  if (!context.req.session.userId) {
+    throw new Error("You need to be signed in");
+  }
+
+  return next();
+};
